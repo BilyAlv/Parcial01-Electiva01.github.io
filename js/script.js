f237a6a402f0dc85bcf559dbcc57b9fe8d8de69e
BilyAlv <bilymanuelm000@gmail.com>
2025-02-12 18:19:46 -0400

    cambios

--- js/script.js
+++ js/script.js
@@ -1,3 +1,3 @@
 document.getElementById("githubButton").addEventListener("click", function() {
-    window.location.href = "https://github.com/tu-usuario/tu-repositorio";
+    window.location.href = "https://github.com/BilyAlv/Parcial01-Electiva01.github.io";
 });
